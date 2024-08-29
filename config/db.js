ec41bcad33059d9896ba92efc91a9f79a9494c1a
Prabal Bhandary <109888062+prabalbhandary@users.noreply.github.com>
2024-08-29 16:17:34 +0545

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = async () => {
-  const url = process.env.MONGO_URL
+  const url = process.env.MONGO_URL || "mongodb+srv://prabal:prabal@prabal.fbo3j.mongodb.net/Doctor_Appointment_App"
   try {
     await mongoose.connect(url);
     console.log(`Mongodb connected ${mongoose.connection.host}`.bgMagenta.white);
